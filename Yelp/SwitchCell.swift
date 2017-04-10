fc4543eeea4a028aeecf07b2f9dd8bfeb88af5bf
Bhalla, Kapil <Kapil_Bhalla@intuit.com>
2017-04-09 21:59:33 -0700

    **This checkin contains the following:** 1. ...
    **Jira ID(s)**
    **Description**
    
    **Pull Request** -
    
    **Dev unit tests executed:**
    1.
    2.
    
    **Suggested QA tests to be covered along with impacted:**
    1.
    2.

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,47 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Bhalla, Kapil on 4/8/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+
+// This handles the
+@objc protocol SwitchCellDelegate {
+    
+    // [KapiL] Why is this objc required ???
+    @objc optional func switchCell(switchCell: SwitchCell, didChangeValue value: Bool)
+    
+}
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!
+    
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func onSwitchTap(_ sender: Any) {
+        
+        print ("switch value changed")
+        
+        //
+        delegate?.switchCell?(switchCell: self, didChangeValue: onSwitch.isOn)
+    }
+}
